e29bd61aca43e6a5df5ac723a95dcf4c7c42ccfc
aleferreiranogueira <ale.ferreiranogueira@gmail.com>
2019-08-20 20:24:33 -0300

    Add hash_string function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,18 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for Identicon.
-  """
+  def main(input) do
+    input |> hash_string
+  end
 
   @doc """
-  Hello world.
+  Hashes the input string and returns a list representation
 
   ## Examples
-
-      iex> Identicon.hello()
-      :world
-
+      iex> Identicon.hash_string("example")
+      [26, 121, 164, 214, 13, 230, 113, 142, 142, 91, 50, 110, 51, 138, 229, 51]
   """
-  def hello do
-    :world
+  @spec hash_string(String.t()) :: list
+  def hash_string(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list()
   end
 end
